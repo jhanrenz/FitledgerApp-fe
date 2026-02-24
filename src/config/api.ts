bca81398d900f63a8040861762571fe5e534e76d
Jhan Renz <jhanrenzventura@gmail.com>
2026-02-24 13:18:51 +0800

    up

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,19 +3,20 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL_API;
 
 export const api = axios.create({
-    baseURL: BASE_URL,
-    headers : {
-        "Accept" : "application/json",
-        "Content-Type" : "application/json",
-        Authorization : `Bearer ${localStorage.getItem('token') || ""}`
-    },
+  baseURL: BASE_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config)=>{
-    const token = localStorage.getItem("token");
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+// Automatically attach token if exists
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
